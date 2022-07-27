b94b818ebb22723a994a27b0ee134bcc125dde21
James A <jqdrqgnq@users.noreply.github.com>
2022-07-27 16:24:24 -0400

    Fix event names.
    Linter fixes.

--- react/features/base/config/configType.ts
+++ react/features/base/config/configType.ts
@@ -305,6 +305,7 @@ export interface IConfig {
     };
     firefox_fake_device?: string;
     flags?: {
+        ssrcRewritingEnabled: boolean;
     };
     focusUserJid?: string;
     gatherStats?: boolean;
